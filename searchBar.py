38482f8a08d72c30a038e9b1eaca3c9d38efd287
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2025-02-04 08:57:01 +0100

    search in progress

--- searchBar.py
+++ searchBar.py
@@ -19,7 +19,7 @@ def cleanWordsInSearch(search: str, words: []) -> str:
 
 def vectorizeSearch(search: str, words_idf: dict) -> dict:
     words = words_idf[list(words_idf.keys())[0]].keys()
-    searchCleaned = cleanWordsInSearch(search, words)
+    searchCleaned = cleanWordsInSearch(search, words) + " "
     searchTF = tf_idf.getTFFromText(searchCleaned, words)
     searchTF_IDF = {}
     for word in words:
@@ -27,8 +27,50 @@ def vectorizeSearch(search: str, words_idf: dict) -> dict:
     return searchTF_IDF
 
 
+def scalar(vector1: dict, vector2: dict) -> float:
+    result = 0
+    for key in vector1.keys():
+        result += vector1[key] * vector2[key]
+    return result
+
+
+def norm(vector: dict) -> float:
+    result = 0
+    for key in vector.keys():
+        result += vector[key] ** 2
+    return result ** 0.5
+
+
+def similarity(vector1: dict, vector2: dict) -> float:
+    return scalar(vector1, vector2) / (norm(vector1) * norm(vector2))
+
+
+def extractVectorFromTF_IDF(tf_idf: dict) -> dict:
+    result = {}
+    for word in tf_idf.keys():
+        result[word] = tf_idf[word]["tf-idf"]
+    return result
+
+
 search = getSearch()
 search = cleanSearch(search)
-words_idf = tf_idf.getMatrix_TF_IDF()
-searchVector = vectorizeSearch(search, words_idf)
-print(searchVector)
+
+
+def getAnswerFromSearch():
+    global answer, fileName
+    words_idf = tf_idf.getMatrix_TF_IDF()
+    searchVector = vectorizeSearch(search, words_idf)
+    for word in searchVector:
+        if searchVector[word] != 0:
+            print(word, ":", searchVector[word])
+    answer = {}
+    for fileName in words_idf.keys():
+        fileVector = extractVectorFromTF_IDF(words_idf[fileName])
+        answer[fileName] = similarity(searchVector, fileVector)
+    return answer
+
+
+answer = getAnswerFromSearch()
+
+for fileName in answer:
+    print(fileName, ":", answer[fileName])
